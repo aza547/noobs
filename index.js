8ef00d8b8b545aa1ec8d6e5ea7f4d35f486a8b3f
aza547 <alexander-12345@hotmail.co.uk>
2025-10-25 11:18:32 +0100

    try fix path issue

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
-process.env.Path += ';';
-process.env.Path += path.resolve(__dirname, 'dist', 'bin').replace('app.asar', 'app.asar.unpacked');
+process.env.Path = path
+  .resolve(__dirname, 'dist', 'bin')
+  .replace('app.asar', 'app.asar.unpacked');
 
 const packageName = 'noobs.node';
 const noobs = require(`./dist/${packageName}`);
