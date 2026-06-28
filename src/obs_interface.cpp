d77def55792b73c2d9d212378dc3050d5b9b533f
aza547 <alexander-12345@hotmail.co.uk>
2026-06-28 13:24:30 +0100

    debug logs

--- src/obs_interface.cpp
+++ src/obs_interface.cpp
@@ -604,7 +604,9 @@ void ObsInterface::output_signal_handler(void *data, calldata_t *cd) {
   blog(LOG_INFO, "Handling %s signal from libobs", ctx->id.c_str());
 
   if (ctx->id == "converted") {
+    blog(LOG_INFO, "1111");
     const char *path = calldata_string(cd, "path");
+    blog(LOG_INFO, "2222 %s", path);
 
     sd = new SignalData{ 
       "output", 
@@ -614,6 +616,7 @@ void ObsInterface::output_signal_handler(void *data, calldata_t *cd) {
       std::nullopt, // Never expect errors here.
       std::string(path),
     };
+    blog(LOG_INFO, "3333");
   } else {
     long long code = calldata_int(cd, "code");
     const char *err = calldata_string(cd, "last_error");
@@ -632,7 +635,9 @@ void ObsInterface::output_signal_handler(void *data, calldata_t *cd) {
     };
   }
 
+  blog(LOG_INFO, "444");
   self->jscb.NonBlockingCall(sd, call_jscb);
+  blog(LOG_INFO, "555");
 }
 
 void ObsInterface::connect_signal_handlers(obs_output_t *output) {
